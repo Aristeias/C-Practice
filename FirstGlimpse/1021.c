86fe5e05fb63695db508285e18049d4fee677a6b
Aristeias <wang1192170106@gmail.com>
2022-07-03 15:21:17 +0800

    初窥门径-综合测验1

--- FirstGlimpse/1021.c
+++ FirstGlimpse/1021.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-    float x;
-    scanf("%f", &x);
-    for(int i = 0; i < x; i++){
-        
+    float a;
+    scanf("%f", &a);
+    double x2 = 1;
+    while(1){
+         x2 = (x2 + a/x2) / 2;
+         if(x2 - sqrt(a)<= 0.00001){
+            break;
+         }
     }
+    printf("%.3f", x2);
 }
