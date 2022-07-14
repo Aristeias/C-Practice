2b61ceb37aad90bdd22e6f964bfed0443a082598
Aristeias <wang1192170106@gmail.com>
2022-07-14 16:06:28 +0800

    添加注释

--- WangDao/Queue/SqQueue.c
+++ WangDao/Queue/SqQueue.c
@@ -36,11 +36,26 @@ int main(){
 
 }
 
+/**
+ * @brief 初始化顺序队列
+ * 
+ * @param Q 队列指针
+ * @return true 
+ * @return false 
+ */
 bool InitQueue(SqQueue *Q){
     Q->front = Q->rear = Q->size = 0;
     return true;
 }
 
+/**
+ * @brief 入队
+ * 
+ * @param Q 队列指针
+ * @param x 值
+ * @return true 
+ * @return false 
+ */
 bool EnQueue(SqQueue *Q, ElemType x){
     if(Q->size == MaxSize){
         printf("顺序队列已满！无法插入值：%d\n", x);
@@ -52,6 +67,14 @@ bool EnQueue(SqQueue *Q, ElemType x){
     return true;
 }
 
+/**
+ * @brief 离队
+ * 
+ * @param Q 队列指针
+ * @param x 值
+ * @return true 
+ * @return false 
+ */
 bool DeQueue(SqQueue *Q, ElemType *x){
     if(Q->size == 0){
         printf("队列为空，无法离队！\n");
@@ -62,6 +85,11 @@ bool DeQueue(SqQueue *Q, ElemType *x){
     return true;
 }
 
+/**
+ * @brief 输出顺序队列
+ * 
+ * @param Q 队列指针
+ */
 void PrintQueue(SqQueue *Q){
     for (int i = Q->front; i < Q->rear; i++)
     {
@@ -70,6 +98,14 @@ void PrintQueue(SqQueue *Q){
     printf("\n");
 }
 
+/**
+ * @brief 获取队列首元素
+ * 
+ * @param Q 队列指针
+ * @param x 首元素地址
+ * @return true 
+ * @return false 
+ */
 bool GetHead(SqQueue *Q, ElemType *x){
     if(Q->size == 0){
         return false;
